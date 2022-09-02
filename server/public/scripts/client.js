16c46ea915173b47109d757d5ce36fac11a947eb
Blake Smith <blake.smith.mn@gmail.com>
2022-09-02 13:34:16 -0500

    calculator front end setup

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,26 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('JQ');
+    $('#submitButton').on('click', submitCalculation)
+    $('#addButton').on('click', chooseType)
+    $('#subtractButton').on('click', chooseType)
+    $('#multiplyButton').on('click', chooseType)
+    $('#divideButton').on('click', chooseType)
+
+}
+let operation = 0;
+function chooseType() {
+    operation = $(this).text();
+    console.log(operation);
+}
+
+function submitCalculation() {
+    let valueOne = $('#valueOne').val();
+    let valueTwo = $('#valueTwo').val();
+    let calculatorInfo = {
+        numberOne: valueOne,
+        type: operation,
+        numberTwo: valueTwo,
+    }
+    console.log(calculatorInfo);
 }
