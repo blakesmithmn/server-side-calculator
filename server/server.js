79005d1f7717201131c650344ac2479b5ac84b23
Blake Smith <blake.smith.mn@gmail.com>
2022-09-01 20:25:50 -0500

    server boilerplate / css connected / html boilerplate

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const PORT = 5000;
+
+app.use(express.static('server/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.listen(PORT, () => {
+//     // showing server is running 
+//     console.log('listening on port', PORT)
+// });
+
+app.listen(PORT, () => {
+    console.log(`listening on http://localhost:${PORT}`);
+})
