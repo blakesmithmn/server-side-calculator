21927e0d337084918548e81e0937277c10730f05
Blake Smith <blake.smith.mn@gmail.com>
2022-09-03 17:11:35 -0500

    results and history showing on DOM after get request / render function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -11,6 +11,7 @@ function onReady() {
 }
 
 let operation = 0;
+let history = [];
 function chooseType() {
     operation = $(this).text();
     console.log(operation);
@@ -44,9 +45,22 @@ function fetchResults() {
     $.ajax({
         method: 'GET',
         url: '/submit',
-    }).then(function (results) {
+    }).then(function (history) {
         // making sure we get back an object that makes sense
-        console.log(results);
-
+        console.log(history);
+        renderResults(history);
     })
+}
+
+function renderResults(history) {
+    $('#history').empty();
+    let last = history[history.length - 1];
+    for (let i = history.length - 1; i >= 0; i--) {
+        $('#history').append(`
+            <li> ${history[i].numberOne} ${history[i].type} ${history[i].numberTwo} = ${history[i].result}  </li>
+        `);
+        console.log('appended');
+        $('#result').text(`${last.result}`);
+    }
+
 }
